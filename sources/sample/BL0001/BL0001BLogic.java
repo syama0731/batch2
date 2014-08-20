e89e5082d22e54cfb38ec932f5bd68dadbb072ae
syama0731 <sandenmanari@gmail.com>
2014-08-21 07:44:59 +0900

    db collector work

--- sources/sample/BL0001/BL0001BLogic.java
+++ sources/sample/BL0001/BL0001BLogic.java
@@ -4,10 +4,15 @@ import jp.terasoluna.fw.batch.openapi.BLogic;
 import jp.terasoluna.fw.batch.openapi.BLogicResult;
 import jp.terasoluna.fw.batch.openapi.ReturnCode;
 
-public class BL0001 implements BLogic<BL0001Data, BL0001JobContext> {
+public class BL0001BLogic implements BLogic<BL0001Data, BL0001JobContext> {
 
 	@Override
 	public BLogicResult execute(BL0001Data data, BL0001JobContext jobContext) {
+		
+		System.out.println(data.getId());
+		System.out.println(data.getItem());
+		System.out.println(data.getPrice());
+		System.out.println(data.getCustomer());
 		
 		return new BLogicResult(ReturnCode.NORMAL_CONTINUE);
 	}
--- sources/sample/BL0001/BL0001Data.java
+++ sources/sample/BL0001/BL0001Data.java
@@ -1,5 +1,47 @@
 package sample.BL0001;
 
 public class BL0001Data {
+	
+	private int id;
+	
+	private String item;
+	
+	private int price;
+	
+	private String customer;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	
+	
 
 }
